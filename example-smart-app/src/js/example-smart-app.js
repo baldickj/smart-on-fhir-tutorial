f0ea010cdbabac587e9a8ac53944ef0575c9ab71
baldickj <41343798+baldickj@users.noreply.github.com>
2024-10-08 08:26:12 -0500

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -52,7 +52,7 @@ function sendToEMR() {
         },
         'type': {
             'coding': [
-{ "coding": [ { "system": "https://fhir.cerner.com/9dbb03d5-622d-4631-bd69-c97ef6942d65/codeSet/72", "code": "1046116289" } ] }
+{ "coding": [ { "system": "https://fhir-ehr.sandboxcerner.com/9dbb03d5-622d-4631-bd69-c97ef6942d65/codeSet/72", "code": "1046116289" } ] }
             ]
         },
         'author': [
@@ -90,7 +90,7 @@ function sendToEMR() {
     };
 
     // The FHIR API endpoint URL (replace with the correct URL)
-    const fhirEndpoint = 'https://fhir-ehr-code.cerner.com/dstu2/9dbb03d5-622d-4631-bd69-c97ef6942d65/DocumentReference';
+    const fhirEndpoint = 'https://fhir-ehr.sandbox.cerner.com/dstu2/9dbb03d5-622d-4631-bd69-c97ef6942d65/DocumentReference';
 
     // Make the POST request to the FHIR server
     fetch(fhirEndpoint, {
